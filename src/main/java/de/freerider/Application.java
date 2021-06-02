eed6999470dee42f627b5d9aee5b7849f4b18a0a
Yakup <ykohne@gmail.com>
2021-06-02 22:56:35 +0200

    Aufgabe 3

--- src/main/java/de/freerider/Application.java
+++ src/main/java/de/freerider/Application.java
@@ -1,6 +1,11 @@
 package de.freerider;
 
+import de.freerider.model.Customer;
+import de.freerider.repository.CrudRepository;
+import de.freerider.repository.CustomerRepository;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,24 +15,99 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        
+        CrudRepository<Customer, String> customerManager = new CustomerRepository();
+        
+        //Create Customers
+        Customer c1 = new Customer("Mühlbauer", "Thomas", "0171");
+        Customer c2 = new Customer("Schmid", "Lisa", "0162");
+        Customer c3 = new Customer("Wolf", "Aldric", "0161");
+        Customer c4 = new Customer("Bohn", "Alf", "0133");
+        Customer c5 = new Customer("Scholz", "Elke", "0178");
+        
+        //
+        ArrayList<Customer> cList = new ArrayList<>(Arrays.asList(c1, c2, c3, c4 ,c5));
+        ArrayList<String> idList = new ArrayList<>();
+        
+        
+        
+        //TEST save / saveAll
+        customerManager.saveAll(cList);
+        
+        System.err.println("Customers:");
+        for (Customer customer : cList) {
+            System.err.println(customer.getId() + ": " + customer.getFirstName());
+            idList.add(customer.getId());
+        }
+        
+        
+        
+        
+        //TEST findAllById / findById
+        Iterable<Customer> testList = customerManager.findAllById(idList);
+        
+        //TEST existsById (should only print true)
+        for (Customer customer: testList) {
+            System.out.println(Boolean.toString(customerManager.existsById(customer.getId())));
+        }
+        //TEST count: Should be 5
+        System.out.println(customerManager.count());
+        
+        
+        
+        
+        //TEST deletes---------
+        customerManager.deleteAll(testList); { //delete and deleteAll
+        
+        //TEST count: Should be 0
+        System.out.println(customerManager.count());
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-
-		};
-	}
-
+        customerManager.saveAll(cList); //Re-add and TEST findAll
+        
+        //TEST count: Should be 5
+        System.out.println(customerManager.count());
+        
+        customerManager.deleteAllById(idList); //deletebyId and deleteAllById
+        
+        //TEST count: Should be 0
+        System.out.println(customerManager.count());
+        
+        customerManager.saveAll(cList); //Re-add and TEST findAll
+        
+        //TEST count: Should be 5
+        System.out.println(customerManager.count());
+        
+        customerManager.deleteAll(); //deleteAll
+        
+        //TEST count: Should be 0
+        System.out.println(customerManager.count());
+        //TEST deletes---------
+        
+        //TEST for if duplicates happen; should still be 5
+        customerManager.saveAll(cList);
+        customerManager.saveAll(cList);
+        System.out.println(customerManager.count());
+        
+    }
+        
+    }
+    
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+            
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+            
+        };
+    }
+    
 }
--- src/main/java/de/freerider/repository/CustomerRepository.java
+++ src/main/java/de/freerider/repository/CustomerRepository.java
@@ -1,13 +1,139 @@
 package de.freerider.repository;
 
+import de.freerider.model.Customer;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Optional;
 import org.springframework.stereotype.Component;
 
 
 @Component
-class CustomerRepository { //implements CrudRepository<Customer, String> {
-	//
-	private final IDGenerator idGen = new IDGenerator( "C", IDGenerator.IDTYPE.NUM, 6 );
-
-
-	
+public class CustomerRepository implements CrudRepository<Customer, String> {
+    //
+    private final IDGenerator idGen = new IDGenerator( "C", IDGenerator.IDTYPE.NUM, 6 );
+    private final  HashMap<String, Customer> customers = new HashMap<>();
+    
+    @Override
+    public <S extends Customer> S save(S entity) {
+        if (entity == null) {
+            throw new IllegalArgumentException("Entitiy can't be null");
+        }
+        String newID = idGen.nextId();
+        
+        if (entity.getId() == null || entity.getId().isEmpty()) {
+            while(existsById(newID)) {
+                newID = idGen.nextId();
+            }
+            entity.setId(newID);
+        }
+        
+        customers.put(entity.getId(), entity);
+        return entity;
+    }
+    
+    @Override
+    public <S extends Customer> Iterable<S> saveAll(Iterable<S> entities) {
+        if (entities == null) {
+            throw new IllegalArgumentException("Entities can't be null");
+        }
+        
+        for (S entity : entities) {
+            if(entity != null){
+                save(entity);
+            }
+        }
+        return entities;
+    }
+    
+    @Override
+    public Optional<Customer> findById(String id) {
+        if (id == null) {
+            throw new IllegalArgumentException("ID can't be null");
+        }
+        Optional<Customer> customer = Optional.ofNullable(customers.get(id));
+        return customer;
+    }
+    
+    @Override
+    public boolean existsById(String id) {
+        if (id == null) {
+            throw new IllegalArgumentException("ID can't be null");
+        }
+        if(customers.containsKey(id)) {
+            return true;
+        }
+        return false;
+    }
+    
+    @Override
+    public Iterable<Customer> findAll() {
+        ArrayList<Customer> allCustomers = new ArrayList<>();
+        customers.forEach((key, value) -> allCustomers.add(value));
+        return allCustomers;
+    }
+    
+    @Override
+    public Iterable<Customer> findAllById(Iterable<String> ids) {
+        if (ids == null) {
+            throw new IllegalArgumentException("IDs can't be null");
+        }
+        ArrayList<Customer> c = new ArrayList<>();
+        
+        for (String id: ids) {
+            Optional<Customer> optional = findById(id);
+            c.add(optional.get());
+        }
+        return c;
+    }
+    
+    @Override
+    public long count() {
+        return customers.size();
+    }
+    
+    @Override
+    public void deleteById(String id) {
+        if (id == null) {
+            throw new IllegalArgumentException("ID can't be null");
+        }
+        customers.remove(id);
+        
+    }
+    
+    @Override
+    public void delete(Customer entity) {
+        if (entity == null) {
+            throw new IllegalArgumentException("Entity can't be null");
+        }
+        customers.remove(entity.getId());
+    }
+    
+    @Override
+    public void deleteAllById(Iterable<? extends String> ids) {
+        if (ids == null) {
+            throw new IllegalArgumentException("IDs can't be null");
+        }
+        for (String id: ids) {
+            deleteById(id);
+        }
+    }
+    
+    @Override
+    public void deleteAll(Iterable<? extends Customer> entities) {
+        if (entities == null) {
+            throw new IllegalArgumentException("Entities can't be null");
+        }
+        for(Customer customer: entities) {
+            delete(customer);
+        }
+    }
+    
+    @Override
+    public void deleteAll() {
+        customers.clear();
+    }
+    
+    
+    
 }
